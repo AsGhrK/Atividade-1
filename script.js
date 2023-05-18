bf35e52b17e0bc23a512623a0e249d7cd7f533d0
AsGhrK <131817273+AsGhrK@users.noreply.github.com>
2023-05-18 10:26:06 -0400

    estilizei e arrumei o script

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ class Produtos{
 
 mostrarDados(){
     return`
+    <div class="produto-padrao">
     <h1>${this.nome}</h1>
     <div>${this.descricao}</div>
-    <h3>${this.preco}</h3>
-    <p>${ this.dataDCadrastro}</p> `
+    <h3>Preço: ${this.preco},00</h3>
+    <p>${ this.dataDCadrastro}</p> 
+    </div>`
     //return "Produto: " +  this.nome + " " + "Data de Cadastro: " + this.dataDCadrastro + " " + "Descrição: " + this.descricao + " " + "Preço: " + this.preco
 }
 }
@@ -25,11 +27,13 @@ class ProdutoDestaque extends Produtos{
     }
     mostrarDadosDestaque(){
         return`
+        <div class="produto-destaque">
         <h1>${this.nome}</h1>
         <img src="${this.imagem}">
         <div>${this.descricao}</div>
-        <h3>${this.preco}</h3>
-        <p>${ this.dataDCadrastro}</p> `
+        <h3>Preço: ${this.preco},50</h3>
+        <p>${this.dataDCadrastro}</p>
+        </div> `
        // return "Produto: " +  this.nome + " " + "Data de Cadastro: " + this.dataDCadrastro + " " + "Descrição: " + this.descricao + " " + "Preço: " + this.preco + " " + this.imagem
     }
     
@@ -39,12 +43,18 @@ class ProdutoDestaque extends Produtos{
 
 
 const produto = new Produtos ("Sabão de coco", "22/08/2023", "Sabão feito de coco" , 2)
+const produto2 = new Produtos ("Sabão de maracuja ", "16/08/2023", "Sabão feito de maracuja" , 2)
+const produto3 = new Produtos ("Sabão de limão ", "16/08/20230", "Sabão feito de limão" , 2)
 const produtoDestaque = new ProdutoDestaque ("Sabão de Laranja", "22/08/2023", "Sabão feito de Laranja" , 2 , "https://blog.sodabel.com.br/wp-content/uploads/2022/11/sabao-de-laranja.jpg" )
 console.log(produto.mostrarDados())
+console.log(produto2.mostrarDados())
 console.log(produtoDestaque.mostrarDadosDestaque())
 
 const produtoPadrao = document.getElementById("lista-produtos")
 produtoPadrao.insertAdjacentHTML('afterbegin', produto.mostrarDados())
-
+const produtoPadrao2 = document.getElementById("lista-produtos")
+produtoPadrao.insertAdjacentHTML('afterbegin', produto2.mostrarDados())
+const produtoPadrao3 = document.getElementById("lista-produtos")
+produtoPadrao.insertAdjacentHTML('afterbegin', produto3.mostrarDados())
 const produtoDestaqueMostrar = document.getElementById("produto-destaque")
 produtoDestaqueMostrar.insertAdjacentHTML('afterbegin', produtoDestaque.mostrarDadosDestaque())
